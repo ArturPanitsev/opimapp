e12c81820eb264a471dd68e68390415c636ed01b
ArturPanitsev <114880871+ArturPanitsev@users.noreply.github.com>
2022-10-04 19:38:10 +0300

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -1,8 +1,17 @@
+const body = document.querySelector('body');
+body.style.pointerEvents = 'none';
+body.style.overflow = 'hidden';
+body.style.userSelect = 'none';
+
+
 const preloader = document.getElementById('preloader');
 
 window.onload = () => {
     preloader.style.animation = 'endLoad 2s';
+    body.style.pointerEvents = 'auto';
+    body.style.overflow = 'auto';
+    body.style.userSelect = 'all';
     setTimeout(()=>{
         preloader.style.display = 'none';
     }, 2000)
-}
+} 
